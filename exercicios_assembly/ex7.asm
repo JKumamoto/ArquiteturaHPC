9a1bdcdd3033498ba9d172b885ebf71f400849c7
Jefferson <jeff.kmt@gmail.com>
2016-07-14 22:48:41 -0300

    exercicios assembly

---
+++ exercicios_assembly/ex7.asm
@@ -0,0 +1,28 @@
+
+; You may customize this and other start-up templates; 
+; The location of this template is c:\emu8086\inc\0_com_template.txt
+
+org 100h
+
+; add your code here
+                     
+MOV DL, 5
+MOV CL, 3 
+MOV BL, 0
+MOV CH, 0
+
+JNZ for
+
+for:
+    ADD BL, DL
+    INC CH
+    CMP CH, CL
+    JNZ for
+          
+MOV AX, BX
+
+ret
+
+
+
+
---
+++ exercicios_assembly/ex_entrega.asm
@@ -0,0 +1,31 @@
+
+; You may customize this and other start-up templates; 
+; The location of this template is c:\emu8086\inc\0_com_template.txt
+
+org 100h
+
+; add your code here
+MOV AX, 38
+MOV CX, 5
+MOV DL, 0
+MOV DH, 0
+
+CMP AX, CX
+JGE for
+JMP resto
+
+for:
+    SUB AX, CX
+    INC DL
+    CMP AX, CX
+    JGE for
+    JMP resto
+    
+resto:
+    MOV DH, AL
+
+ret
+
+
+
+
